e381647cf02df6f6d1b90313af4469cc24d61a01
RPCYBER <125120908+aisfrog@users.noreply.github.com>
2023-03-11 12:06:11 +0200

    Rpcyber/add logger (#1)
    * add logger for commander
    
    * unversioned files
    
    * base_version
    
    * Delete .gitignore
    
    * Delete botnet-commander.iml
    
    * Delete profiles_settings.xml
    
    * Delete misc.xml
    
    * Delete modules.xml
    
    * Delete vcs.xml
    
    ---------
    
    Co-authored-by: RPCYBER <contact@rpcyber.pro>

--- commander/commander.py
+++ commander/commander.py
@@ -1,5 +1,5 @@
 import os
-import re
+import asyncio
 import uuid
 from pathlib import Path
 import time
---
+++ commander/logger.py
@@ -0,0 +1,24 @@
+import logging
+from os import path
+
+
+class CoreLogger:
+    def __init__(self, log_level, log_dir, log_name):
+        self.core = logging.getLogger("CORE")
+        self.core.setLevel(log_level)
+        self.log_path = path.join(log_dir, log_name)
+        if not path.isfile(self.log_path):
+            try:
+                open(self.log_path, 'a').close()
+            except PermissionError:
+                print("Running RATServer requires root like privileges, permission denied when creating path for log"
+                      " file. Please run as sudo!")
+                exit(3)
+            except Exception as err:
+                print("Unexpected error has occurred while creating the logger for server: {}".format(err))
+                exit(4)
+        self.fh = logging.FileHandler(self.log_path)
+        self.fh.setLevel(log_level)
+        self.log_formatter = logging.Formatter('%(asctime)s - [%(name)s]::%(levelname)s - %(message)s')
+        self.fh.setFormatter(self.log_formatter)
+        self.core.addHandler(self.fh)
