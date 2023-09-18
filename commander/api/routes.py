c8af8cc2dd1d322cc3b7361e916e0f213057ed59
RPCYBER <contact@rpcyber.pro>
2023-09-18 23:23:00 +0300

    WIP: need to decide how to proceed with API, the only way to extract info in routes is to import bot_commander object like in main and have methods for what API must do (get data, send data, etc)

--- commander/api/routes.py
+++ commander/api/routes.py
@@ -7,6 +7,4 @@ router = APIRouter()
 
 @router.get("/agents/count", status_code=HTTP_200_OK)
 async def agents_count():
-    # self.logger.core.info("Endpoint accessed: /agents/count")
-    # return len(self.agents)
     return "Yes"
