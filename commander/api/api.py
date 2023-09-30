3764c8279cbba1910889fbf71fdede3d5b3f4126
RPCYBER <contact@rpcyber.pro>
2023-09-30 20:52:46 +0300

    WIP: added pki_path, small changes to have https API, and then restructure and move all from routes to something else, like components and start coding endpoints

--- commander/api/api.py
+++ commander/api/api.py
@@ -1,3 +1,4 @@
+import os
 import uvicorn
 import asyncio
 from fastapi import FastAPI
@@ -8,15 +9,22 @@ app = FastAPI()
 
 class CommanderApi:
 
-    def __init__(self, host, port, prefix, log_level, api_router):
+    def __init__(self, host, port, prefix, log_level, pki_path, api_router):
         self.addr = host
         self.port = port
         self.prefix = prefix
-        self.api_router = api_router
+        self.pki_path = pki_path
         self.log_level = log_level
+        self.api_router = api_router
+        self.api_cert = "api-cert.pem"
+        self.api_key = "api-key.pem"
 
     def __run_server(self):
         uvicorn.run(app, host=self.addr, port=self.port, log_level=self.log_level)
+
+    def __api_certs_gen(self):
+        api_cert_path = os.path.join(self.pki_path, self.api_cert)
+        api_key_path = os.path.join(self.pki_path, self.api_key)
 
     async def start_api(self):
         app.include_router(self.api_router, prefix=self.prefix)
--- commander/api/routes.py
+++ commander/api/routes.py
@@ -5,8 +5,8 @@ from commander.server.bot_commander import BotCommander
 from commander.helpers.helper import load_conf
 
 
-(HOST, PORT, LOG_LEVEL, LOG_DIR, LOG_NAME, OFFLINE_TOUT, CMD_TOUT, RESP_WAIT_WINDOW, API_HOST, API_PORT, API_PREFIX,
- API_LOG_LEVEL) = load_conf()
+(HOST, PORT, LOG_LEVEL, LOG_DIR, LOG_NAME, OFFLINE_TOUT, CMD_TOUT, PKI_PATH, RESP_WAIT_WINDOW, API_HOST, API_PORT,
+ API_PREFIX, API_LOG_LEVEL) = load_conf()
 
 bot_server = BotCommander(HOST, PORT, LOG_LEVEL, LOG_DIR, LOG_NAME, OFFLINE_TOUT, CMD_TOUT, RESP_WAIT_WINDOW)
 
@@ -18,4 +18,4 @@ def count_agents() -> int:
     return bot_server.count_connected_agents()
 
 
-api = CommanderApi(API_HOST, API_PORT, API_PREFIX, API_LOG_LEVEL, router)
+api = CommanderApi(API_HOST, API_PORT, API_PREFIX, API_LOG_LEVEL, PKI_PATH, router)
