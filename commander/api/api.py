5fdfc07a8b5af1599a4a2d2097287132442cc488
RPCYBER <contact@rpcyber.pro>
2023-10-15 22:17:11 +0300

    WIP: start coding endpoints, decide when agents are online/offline/counted, if agent changes port don't update DB like it is now

--- commander/api/api.py
+++ commander/api/api.py
@@ -1,11 +1,7 @@
-import os
-import sys
 import uvicorn
 import asyncio
 import logging
-from cryptography import x509
 from fastapi import FastAPI
-from signal import SIGTERM
 
 
 app = FastAPI()
@@ -19,44 +15,14 @@ class CommanderApi:
         self.prefix = prefix
         self.pki_path = f"{base_path}/pki"
         self.api_router = api_router
-        self.api_key = "api-key.pem"
-        self.api_cert = "api-cert.pem"
+        self.api_key_fp = f"{self.pki_path}/api-key.pem"
+        self.api_cert_fp = f"{self.pki_path}/api-cert.pem"
         self.api_log_level = uvicorn_log_level
         self.logger = logging.getLogger(__name__+"."+self.__class__.__name__)
 
     def __run_server(self):
-        self.__check_certs_exist()
-        uvicorn.run(app, host=self.addr, port=self.port, log_level=self.api_log_level, ssl_certfile="", ssl_keyfile="")
-
-    def __check_certs_exist(self):
-        cert_path = os.path.join(self.pki_path, self.api_cert)
-        key_path = os.path.join(self.pki_path, self.api_key)
-        if os.path.isfile(cert_path) and os.path.isfile(key_path):
-            self.logger.debug(f"Certificate files {self.api_key} and {self.api_cert} have been found in {self.pki_path}"
-                              f". Attempting to load and check if issuer is the same CA used by Commander")
-            self.__certs_check_issuer(cert_path, key_path)
-        else:
-            self.logger.debug(f"Certificate files have not been found in {self.pki_path}, attempting to create them")
-            self.__certs_generate()
-
-    def __certs_check_issuer(self, cert_path, key_path):
-        for file in cert_path, key_path:
-            try:
-                fh = open(file, 'b')
-                x509.load_pem_x509_certificate(fh.read())
-            except ValueError:
-                self.logger.error(f"Certificate file {file} is not a valid pem file or is malformed. Please ensure that"
-                                  f"the pem certificate file is valid. You can remove them and Commander will generate"
-                                  f" new ones at startup")
-                os.kill(os.getpid(), SIGTERM)
-                sys.exit()
-            except Exception as err:
-                self.logger.error(f"Unexpected exception occurred when attempting to check certificates issuer: {err}")
-                os.kill(os.getpid(), SIGTERM)
-                sys.exit()
-
-    def __certs_generate(self):
-        pass
+        uvicorn.run(app, host=self.addr, port=self.port, log_level=self.api_log_level, ssl_certfile=self.api_cert_fp,
+                    ssl_keyfile=self.api_key_fp, ssl_keyfile_password="commander-api")
 
     async def start_api(self):
         app.include_router(self.api_router, prefix=self.prefix)
